4fafc0b0bb9d029ed0a1dfc8b73e9dda7cd389b3
tarmeli <tommi.tarkki@gmail.com>
2018-03-29 16:04:49 +0300

    name -> playerId in match model

--- src/models/match.model.js
+++ src/models/match.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MatchSchema = new mongoose.Schema(
   {
     matchName: String,
-    players: [{ name: String, win: Boolean, tie: Boolean }]
+    players: [{ playerId: String, win: Boolean, tie: Boolean }]
   },
   {
     timestamps: true
